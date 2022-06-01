9684b7469bab93548baaad403ae11dafdf975b59
Clark Cucinell <clarkc@holly.mcs.anl.gov>
2022-06-01 13:54:38 -0500

    switching to correct localize params function

--- service-scripts/App-RNASeq2.pl
+++ service-scripts/App-RNASeq2.pl
@@ -117,8 +117,8 @@ sub process_rnaseq {
     print STDERR "$tmpdir\n";
     ###localize_params for regular script
     #localize_params_local for testing: will not download files
-    #$params = localize_params($tmpdir, $params);
-    $params = localize_params_local($tmpdir, $params);
+    $params = localize_params($tmpdir, $params);
+    #$params = localize_params_local($tmpdir, $params);
     
     my @outputs;
     my $prefix = $recipe;
