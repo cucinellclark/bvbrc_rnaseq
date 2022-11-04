47c02b0d52ecd0319d220dfa50be3dcfaa49f1af
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-11-04 13:30:28 -0500

    added optional trimming

--- scripts/run_rnaseq.py
+++ scripts/run_rnaseq.py
@@ -31,10 +31,10 @@ def main(genome_list, experiment_dict, tool_params, output_dir, comparisons, ses
 
     # Trimming
     # TODO: replace threads with tool_params value
-    if 
-    for condition in experiment_dict:
-        for sample in experiment_dict[condition].get_sample_list():
-            preprocess.run_trimming(sample, 8)
+    if map_args.trimming: 
+        for condition in experiment_dict:
+            for sample in experiment_dict[condition].get_sample_list():
+                preprocess.run_trimming(sample, 8)
 
     ### Sampled align against one genome?
     # TODO: replace threads with tool_params value
