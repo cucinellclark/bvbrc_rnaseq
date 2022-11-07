e660c7de18f77df71211b1e23ec9856c07df0264
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-11-07 10:27:26 -0600

    testing

--- scripts/rnaseq_grid_violin_plots.R
+++ scripts/rnaseq_grid_violin_plots.R
@@ -53,6 +53,7 @@ system.map <- read.table(mapping.file,sep="\t",header=T,stringsAsFactors=FALSE)
 #Filter entries with no system label and get the intersection of patric_ids
 system.map = system.map[!grepl("NONE",system.map[,2]),]
 keep.rows <- which(rownames(counts.mtx) %in% system.map[,2])
+print(keep.rows)
 counts.mtx = counts.mtx[keep.rows,]
 print(head(system.map))
 print('counts 2')
