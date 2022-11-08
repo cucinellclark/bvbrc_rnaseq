d92a5f43c96e1eeff2227f47dc372616e0b6e7df
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2022-11-08 11:15:05 -0600

    testing removing added R-library path

--- scripts/run_rnaseq.py
+++ scripts/run_rnaseq.py
@@ -212,8 +212,8 @@ if __name__ == "__main__":
     print("map_args:\n{0}".format(map_args))
 
     # set path to use correct version of samtools
-    os.environ['PATH'] = "/disks/patric-common/runtime/samtools-1.9/bin:"+os.environ['PATH']
-    os.environ['R_LIBS'] = "/disks/patric-common/runtime/lib/R/library:"+os.environ['R_LIBS']
+    #os.environ['PATH'] = "/disks/patric-common/runtime/samtools-1.9/bin:"+os.environ['PATH']
+    #os.environ['R_LIBS'] = "/disks/patric-common/runtime/lib/R/library:"+os.environ['R_LIBS']
     print('R_LIBS path = {0}'.format(os.environ['R_LIBS']))
 
     # output directory
