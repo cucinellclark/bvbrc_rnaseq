4d7a02bbd63cae4a4fa9fb5cfa7699e51f267418
Clark Cucinell <ac.cucinell@holly.mcs.anl.gov>
2023-01-10 13:27:46 -0600

    testing 12

--- scripts/rnaseq_grid_violin_plots.R
+++ scripts/rnaseq_grid_violin_plots.R
@@ -115,7 +115,7 @@ for (i in 1:length(systems)) {
     print("here4")
     for (c in conditions) {
         print(c)
-        print(melt.df$Sample %in% subset(metadata,Condition==c)$Sample)
+        print(sum(melt.df$Sample %in% subset(metadata,Condition==c)$Sample))
         melt.df[melt.df$Sample %in% subset(metadata,Condition==c)$Sample,]$Condition = c
     }
     print("here5")
